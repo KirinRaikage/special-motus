f6b13e9e840a313d6fc6aba5fb39f2cb9c81dca2
Faïd Abdourahim <manmitsu@hotmail.fr>
2018-03-18 14:33:13 +0100

    Classe test pour voir si ça fonctionne. Conformément au CdC, ça fonctionne en fonction du temps en minutes donc il est converti.

--- TestMot.java
+++ TestMot.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
 public class TestMot {
     public static void main(String[] args) {
-        Mot mot=new Mot("azert");
-        System.out.println(mot.getCombinaison());
+
+        //Mot mot=new Mot("azert");
+        //System.out.println(mot.getCombinaison());
         //System.out.println(mot.lettreDansCombi(mot.getCombinaison(),'e'));
         //System.out.println(mot.lettreDansCombi(mot.getCombinaison(), 'i'));
-        System.out.println(mot.nbMalPlace("oiret"));
+        final long tempsActuel =System.currentTimeMillis();
+        //System.out.println(tempsActuel);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Veuillez saisir un mot : ");
+        String saisie=sc.nextLine();
+        final long duree= ((System.currentTimeMillis()-tempsActuel)/1000)/60; //Durée convertie en minutes
+        System.out.println("Il s'est écoulé " + duree + " m entre la fin de la saisie.");
+        //System.out.println(mot.nbMalPlace("oiret"));1
+
 
     }
 }
